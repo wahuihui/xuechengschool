d3b8ac764497a93463f619bdeb71010a03ce627b
hui <1154437939@qq.com>
2023-02-02 16:23:56 +0800

    service模块测试启动类

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/ServiceApplication.java
@@ -0,0 +1,18 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aoHui
+ * @description
+ * @date 2023/2/2 14:12
+ */
+@SpringBootApplication
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}

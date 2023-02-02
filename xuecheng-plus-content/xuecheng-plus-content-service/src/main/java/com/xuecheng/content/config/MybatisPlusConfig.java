85600e167a4c01d1cfa7d53a4927595bd7acd9f4
hui <1154437939@qq.com>
2023-02-02 16:19:47 +0800

    mybatis-plus 配置类

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package com.xuecheng.content.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author aoHui
+ * @description mybatis-plus 配置类
+ * @date 2023/2/2 14:06
+ */
+@Configuration
+@MapperScan("com.xuecheng.content.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 定义分页拦截器
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
+
